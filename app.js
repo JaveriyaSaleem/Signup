119f092b57f441110b612bbfe04239897216011a
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-09-23 10:15:32 +0500

    added

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var dataObject;
+
+var arrayOfDataObj=[]
 document.getElementById("Register").addEventListener("click",function(){
-    var nameGet = document.getElementById('form3Example1cg');
-    var emailGet = document.getElementById('form3Example3cg');
-    var password = document.getElementById('form3Example4cg');
+ var nameGet = document.getElementById('form3Example1cg');
+ var emailGet = document.getElementById('form3Example3cg');
+ var password = document.getElementById('form3Example4cg');
     // console.log(nameGet.value,emailGet.value,password.value)
 
-dataObject = {
+var dataObject = {
     name : nameGet.value,
     email: emailGet.value,
     password: password.value
 }
+arrayOfDataObj.push(dataObject)
 
 console.log(dataObject)
+localStorage.setItem("objectKaSaman",JSON.stringify(arrayOfDataObj))
+console.log(arrayOfDataObj)
+nameGet = "";
+emailGet = "";
+password = "";
 })
-localStorage.setItem("objectKaSaman",JSON.stringify(dataObject))
