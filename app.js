9073395e0791878ec6d95337e80bedf3c708dff8
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-09-25 02:29:02 +0500

    added

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
+var arrayOfDataObj = JSON.parse(localStorage.getItem("objectOfData")) || [];
+var mainCont = document.getElementById('mainContainer');
 
-var arrayOfDataObj=[]
-document.getElementById("Register").addEventListener("click",function(){
- var nameGet = document.getElementById('form3Example1cg');
- var emailGet = document.getElementById('form3Example3cg');
- var password = document.getElementById('form3Example4cg');
-    // console.log(nameGet.value,emailGet.value,password.value)
+document.getElementById("register").addEventListener("click", function() {
+    var nameGet = document.getElementById('fullName');
+    var emailGet = document.getElementById('email');
+    var password = document.getElementById('password');
 
-var dataObject = {
-    name : nameGet.value,
-    email: emailGet.value,
-    password: password.value
-}
-arrayOfDataObj.push(dataObject)
+    var dataObject = {
+        name: nameGet.value,
+        email: emailGet.value,
+        password: password.value
+    };
+    arrayOfDataObj.push(dataObject);
 
-console.log(dataObject)
-localStorage.setItem("objectKaSaman",JSON.stringify(arrayOfDataObj))
-console.log(arrayOfDataObj)
-nameGet = "";
-emailGet = "";
-password = "";
-})
+    console.log(dataObject);
+    localStorage.setItem("objectOfData", JSON.stringify(arrayOfDataObj));
+    console.log(arrayOfDataObj);
+
+    // Clear input fields
+    nameGet.value = "";
+    emailGet.value = "";
+    password.value = "";
+
+    // Dynamically create login form
+    mainCont.innerHTML = `
+        <div class="content"> 
+            <h2>Login Page</h2> 
+            <div class="form"> 
+                <div class="inputBox"> 
+                    <input type="email" required id="email"> 
+                    <i>Email</i> 
+                </div> 
+                <div class="inputBox"> 
+                    <input type="password" required id="password"> 
+                    <i>Password</i> 
+                </div> 
+                <div class="inputBox"> 
+                    <input type="button" value="login" id="login"> 
+                </div> 
+            </div> 
+        </div> 
+    `;
+
+
+    document.getElementById('login').addEventListener("click", function() {
+        var emailGet = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        var loginSuccessful = false; 
+
+        for (var i = 0; i < arrayOfDataObj.length; i++) {
+            if (emailGet === arrayOfDataObj[i].email && password === arrayOfDataObj[i].password) {
+                alert("Login successful!!");
+                loginSuccessful = true;
+                break;
+            }
+        }
+
+        if (!loginSuccessful) {
+            alert("Login failed. Incorrect email or password.");
+        }
+    });
+});
+
+console.log(arrayOfDataObj.length);
